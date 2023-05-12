3c4b890ca97bb39d6dd314b87b8031c2bcfafd30
Austin Griffith <austin@ethereum.org>
2023-05-12 13:35:07 -0600

    read state and only show the right button

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -1,11 +1,17 @@
 import Head from "next/head";
 import type { NextPage } from "next";
 import { useAccount } from "wagmi";
-import { Address } from "~~/components/scaffold-eth";
+import { Address, RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
 import { useScaffoldContractRead, useScaffoldContractWrite } from "~~/hooks/scaffold-eth";
 
 const Home: NextPage = () => {
-  const { address } = useAccount();
+  const { address, isConnected } = useAccount();
+
+  const { data: whenStaked } = useScaffoldContractRead({
+    contractName: "StupidStaker",
+    functionName: "whenStaked",
+    args: [address],
+  });
 
   const { writeAsync: doStake } = useScaffoldContractWrite({
     contractName: "StupidStaker",
@@ -31,19 +37,35 @@ const Home: NextPage = () => {
         <meta name="description" content="Created with 🏗 scaffold-eth-2" />
       </Head>
 
-      <div className="flex items-center flex-col flex-grow pt-10">
-        <div className="px-5">
-          <h1 className="text-4xl font-bold text-center">Stupid Staker</h1>
-          <Address address={address} />
-
-          <div className="text-center">Time Staked: {timeStaked?.toNumber()}s</div>
-          <button className="btn btn-primary" onClick={doStake}>
-            STAKE
-          </button>
-          <button className="btn btn-primary" onClick={undoStake}>
-            UNSTAKE
-          </button>
-        </div>
+      <div className="flex items-center flex-col flex-grow pt-10 gap-2">
+        <h1 className="text-4xl font-bold text-center">Stupid Staker</h1>
+        {isConnected ? (
+          <>
+            <Address address={address} />
+            {!whenStaked?.eq(0) ? (
+              <>
+                <div className="text-center p-8">Time Staked: {timeStaked?.toString()}s</div>
+                <div className="flex  gap-2">
+                  <button className="btn btn-primary" onClick={undoStake}>
+                    UNSTAKE
+                  </button>
+                </div>
+              </>
+            ) : (
+              <>
+                <div className="flex  gap-2">
+                  <button className="btn btn-primary" onClick={doStake}>
+                    STAKE
+                  </button>
+                </div>
+              </>
+            )}
+          </>
+        ) : (
+          <>
+            <RainbowKitCustomConnectButton />
+          </>
+        )}
       </div>
     </>
   );
